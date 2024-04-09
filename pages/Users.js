2985124a59cb842e2266ccdb32509453d8e9b1e0
sulphurninja <sulphurninja@gmail.com>
2024-04-09 13:24:22 +0530

    WOW

---
+++ pages/Users.js
@@ -0,0 +1,185 @@
+import React, { useEffect, useState } from 'react';
+import { FiHome, FiCheckSquare, FiBookOpen, FiTrendingUp, FiMessageSquare, FiHelpCircle, FiCalendar, FiUsers, FiFile, FiFileText, FiEdit, FiTrash2, FiGitPullRequest, FiPhoneOutgoing, FiNavigation2, FiSettings, FiMinusCircle } from 'react-icons/fi';
+import { IoIosNotifications } from "react-icons/io";
+import Link from 'next/link'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../components/ui/accordion';
+import { Input } from '../components/ui/input';
+import { Button } from '../components/ui/button';
+import axios from 'axios';
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerFooter, DrawerTrigger, DrawerClose } from "../components/ui/drawer";
+
+export default function Users() {
+    const [users, setUsers] = useState([]);
+    const [editUser, setEditUser] = useState(null);
+    const [userName, setName] = useState('');
+    const [role, setRole] = useState('');
+    const [balance, setBalance] = useState(0);
+    const [drawerOpen, setDrawerOpen] = useState(false); // State variable for drawer visibility
+
+    useEffect(() => {
+        async function fetchUsers() {
+            const { data } = await axios.get('/api/userlist');
+            setUsers(data);
+        }
+        fetchUsers();
+    }, []);
+
+    const handleEdit = (user) => {
+        setEditUser(user);
+        setName(user.userName);
+        setRole(user.role);
+        setDrawerOpen(true); // Open the drawer when editing
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`/api/userlist?id=${id}`);
+            setUsers(users.filter((user) => user._id !== id));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleUpdateUser = async () => {
+        try {
+            const { data } = await axios.put(`/api/userlist?id=${editUser._id}`, {
+                userName,
+                role,
+                balance
+            });
+            console.log(data);
+            setUsers(
+                users.map((user) =>
+                    user._id === editUser._id ? { ...user, userName, role, balance } : user
+                )
+            );
+            setEditUser(null);
+
+            setName('');
+            setRole('');
+            setBalance(0);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className="flex min-h-screen text-center bg-gray-100">
+            <aside className="w-16 md:w-20 lg:w-24 bg-[#1D1817] text-white" >
+                <nav className="flex flex-col items-center">
+                    <div className="py-4">
+                        <Link href='/admin'>
+
+                            <button className="text-2xl  text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                <FiHome className="lg:ml-1" />
+                                <span className="hidden text-sm font-bold text-center md:inline">Home</span>
+                            </button>
+                        </Link>
+                    </div>
+                    <div className="py-4">
+                        <Link href='/Users'>
+                            <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                <FiUsers className="lg:ml-1" />
+                                <span className="hidden text-sm text-center font-bold md:inline">Users</span>
+                            </button>
+                        </Link>
+                    </div>
+                    <div className="py-4">
+                        <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                            <IoIosNotifications className="lg:ml-3" />
+                            <span className="hidden text-xs font-bold text-center md:inline">Requests</span>
+                        </button>
+                    </div>
+                    <div className="py-4">
+                        <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                            <FiCheckSquare className="lg:ml-3" />
+                            <span className="hidden text-center text-sm font-bold md:inline">Results</span>
+                        </button>
+                    </div>
+                    <div className="py-4">
+                        <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                            <FiSettings className="lg:ml-4" />
+                            <span className="hidden text-sm font-bold md:inline">Settings</span>
+                        </button>
+                    </div>
+                    {/* <div className="py-4">
+                            <Link href='/Attendance'>
+                                <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                    <FiCalendar className="lg:ml-4" />
+                                    <span className="hidden text-sm font-bold md:inline">Calendar</span>
+                                </button>
+                            </Link>
+                        </div> */}
+                    <div className="py-4">
+                        <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                            <FiHelpCircle className="lg:ml-1" />
+                            <span className="hidden text-sm font-bold md:inline">Help</span>
+                        </button>
+                    </div>
+
+                </nav>
+            </aside >
+            <main className="flex-1 bg-neutral-300 p-6">
+                <h1 className='text-xl font-bold'>USERS</h1>
+                <Accordion type="single" collapsible className="w-full">
+                    {users.map((user, index) => (
+                        <AccordionItem key={index} value={`user-${index}`}>
+                            <AccordionTrigger>
+                                <h1 className=''>{user.userName}</h1>
+                            </AccordionTrigger>
+                            <AccordionContent>
+                                <div className='text-start'>
+                                    <p>Username: {user.userName}</p>
+                                    <p>Role: {user.role}</p>
+                                    <p>Balance: {user.balance}</p>
+                                </div>
+                                <div className="flex justify-between items-center mt-2">
+                                    <Drawer >
+
+                                        <DrawerTrigger asChild>
+                                            <Button onClick={() => handleEdit(user)} className="flex items-center  text-white py-1 px-2 rounded-md">
+                                                <FiEdit className="mr-1" />
+                                                Edit
+                                            </Button>
+                                        </DrawerTrigger>
+                                        <DrawerContent>
+                                            <DrawerHeader>
+                                                <DrawerTitle>Edit User</DrawerTitle>
+                                            </DrawerHeader>
+                                            <form className='p-12'>
+                                                <label className=''>
+                                                    Username:
+                                                </label>
+                                                <Input type="text" className='' value={userName} onChange={(e) => setName(e.target.value)} />
+
+                                                <label>
+                                                    Role:
+                                                    <Input type="text" value={role} onChange={(e) => setRole(e.target.value)} />
+                                                </label>
+                                                <label>
+                                                    Balance:
+                                                    <Input type="number" value={balance} onChange={(e) => setBalance(e.target.value)} />
+                                                </label>
+                                                <DrawerFooter>
+                                                    <Button type="button" onClick={handleUpdateUser}>Update</Button>
+                                                    <DrawerClose onClick={() => setDrawerOpen(false)}>Close</DrawerClose>
+                                                </DrawerFooter>
+                                            </form>
+                                        </DrawerContent>
+                                    </Drawer>
+
+                                    <Button onClick={() => handleDelete(user._id)} className="flex items-center bg-red-500 hover:bg-red-800 text-white py-1 px-2 rounded-md">
+                                        <FiTrash2 className="mr-1" />
+                                        Delete
+                                    </Button>
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    ))}
+                </Accordion>
+
+
+            </main>
+        </div>
+    )
+}
---
+++ pages/admin.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import { FiHome, FiCheckSquare, FiBookOpen, FiTrendingUp, FiMessageSquare, FiHelpCircle, FiCalendar, FiUsers, FiFile, FiFileText, FiGitPullRequest, FiPhoneOutgoing, FiNavigation2, FiSettings, FiMinusCircle } from 'react-icons/fi';
+import { IoIosNotifications } from "react-icons/io";
+import Link from 'next/link'
+import Navbar from '../components/globals/Navbar'
+
+export default function Admin() {
+    return (
+        <>
+            {/* <Navbar/> */}
+            <div className="flex min-h-screen text-center bg-gray-100" >
+                <aside className="w-16 md:w-20 lg:w-24 bg-[#1D1817] text-white" >
+                    <nav className="flex flex-col items-center">
+                        <div className="py-4">
+                            <button className="text-2xl  text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                <FiHome className="lg:ml-1" />
+                                <span className="hidden text-sm font-bold text-center md:inline">Home</span>
+                            </button>
+                        </div>
+                        <div className="py-4">
+                            <Link href='/Users'>
+                                <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                    <FiUsers className="lg:ml-1" />
+                                    <span className="hidden text-sm text-center font-bold md:inline">Users</span>
+                                </button>
+                            </Link>
+                        </div>
+                        <div className="py-4">
+                            <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                <IoIosNotifications className="lg:ml-3" />
+                                <span className="hidden text-xs font-bold text-center md:inline">Requests</span>
+                            </button>
+                        </div>
+                        <div className="py-4">
+                            <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                <FiCheckSquare className="lg:ml-3" />
+                                <span className="hidden text-center text-sm font-bold md:inline">Results</span>
+                            </button>
+                        </div>
+                        <div className="py-4">
+                            <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                <FiSettings className="lg:ml-4" />
+                                <span className="hidden text-sm font-bold md:inline">Settings</span>
+                            </button>
+                        </div>
+                        {/* <div className="py-4">
+                            <Link href='/Attendance'>
+                                <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                    <FiCalendar className="lg:ml-4" />
+                                    <span className="hidden text-sm font-bold md:inline">Calendar</span>
+                                </button>
+                            </Link>
+                        </div> */}
+                        <div className="py-4">
+                            <button className="text-2xl text-gray-300 hover:text-[#F56565] transition duration-300 ease-in-out">
+                                <FiHelpCircle className="lg:ml-1" />
+                                <span className="hidden text-sm font-bold md:inline">Help</span>
+                            </button>
+                        </div>
+
+                    </nav>
+                </aside >
+
+                <main className="flex-1   bg-neutral-300  p-6">
+                    <header className="flex items-center justify-between mb-6">
+                        <div className='w-[15%]  h-[10%]'>
+                            {/* <h1 className='font-bold'>Dice - Admin</h1> */}
+                        <img src='/dice.gif' className='h-8' />
+
+                        </div>
+                        
+                        <h1 className="text-lg font-bold text-black font-sans">Hello Admin!</h1>
+
+                        <button
+                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded"
+                        // onClick={handleLogoutClick}
+                        >
+                            Logout
+                        </button>
+                    </header>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+                        {/* Task Card */}
+                        <Link href='/Users'>
+                            <div className="p-4 bg-[#1D1817] rounded-md hover:bg-[#C53131] cursor-pointer transition duration-300 ease-in-out">
+                                <FiCheckSquare className="absolute" />
+                                <h2 className="text-lg font-bold font-sans text-[#FEFEFF] mb-2">Users </h2>
+                                <p className="text-[#D7D3D0] font-sans">Your Users appear here</p>
+                            </div>
+                        </Link>
+                        {/* Tutorial Card */}
+                        <Link href='/Requests'>
+                            <div className="p-4 bg-[#1D1817] rounded-md hover:bg-[#C53131] cursor-pointer transition duration-300 ease-in-out">
+                                <FiBookOpen className="absolute" />
+                                <h2 className="text-lg font-bold font-sans text-[#FEFEFF] mb-2">Requests</h2>
+                                <p className="text-[#D7D3D0] font-sans">Balance Requests</p>
+                            </div>
+                        </Link>
+                        {/* Progress Card */}
+                        <Link href='/Update'>
+                            <div className="p-4 bg-[#1D1817] rounded-md hover:bg-[#C53131] cursor-pointer transition duration-300 ease-in-out">
+                                <FiTrendingUp className="absolute" />
+                                <h2 className="text-lg font-bold font-sans text-[#FEFEFF] mb-2">Update Winning Numbers</h2>
+                                <p className="text-[#D7D3D0] font-sans">Update Results</p>
+                            </div>
+                        </Link>
+                        {/* Discussion Card */}
+                        <Link href='/settings'>
+                            <div className="p-4 bg-[#1D1817] rounded-md hover:bg-[#C53131] cursor-pointer transition duration-300 ease-in-out">
+                                <FiMessageSquare className="absolute" />
+                                <h2 className="text-lg font-bold font-sans text-[#FEFEFF] mb-2">Settings</h2>
+                                <p className="text-[#D7D3D0] font-sans">Update Account settings</p>
+                            </div>
+                        </Link>
+                        {/* <Link href='/Attendance'>
+                            <div className="p-4 bg-[#1D1817] rounded-md hover:bg-[#C53131] cursor-pointer transition duration-300 ease-in-out">
+                                <FiCalendar className="absolute" />
+                                <h2 className="text-lg font-bold font-sans text-[#FEFEFF] mb-2">Attendance</h2>
+                                <p className="text-[#D7D3D0] font-sans">Track your attendance</p>
+                            </div>
+                        </Link> */}
+                        {/* Help Card */}
+                        <Link href='/help'>
+                            <div className="p-4 bg-[#1D1817] rounded-md hover:bg-[#C53131] cursor-pointer transition duration-300 ease-in-out">
+                                <FiHelpCircle className="absolute" />
+                                <h2 className="text-lg font-bold font-sans text-[#FEFEFF] mb-2">Help</h2>
+                                <p className="text-[#D7D3D0] font-sans">Support & Guidance</p>
+                            </div>
+                        </Link>
+
+                    </div>
+                    <footer className='mt-12'>
+
+                        <h1 className='text-black font- mt-10 lg:mt-24'>  COPYRIGHT © GUDI GUDI | DESIGNED BY VIRATCS IT. ALL RIGHTS RESERVED</h1>
+
+                    </footer>
+
+                </main>
+            </div>
+        </>
+    );
+}
